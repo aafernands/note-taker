9ac6d7442b169fafa2b006357f915f214dc1220a
Alex Fernandes <alexfernands@outlook.com>
2020-11-21 15:34:31 -0500

    add POST /api/notes to save notes to db.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 // expose the static  asset folder
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 // Routes
 app.get("/", function (req, res) {
@@ -26,6 +28,23 @@ app.get("/api/notes", function (req, res) {
 	console.log(JSON.parse(notes));
 
 	res.json(JSON.parse(notes));
+});
+
+app.post("/api/notes", function (req, res) {
+	const notes = fs.readFileSync(path.join(__dirname, "./data/db.json"));
+
+	console.log("body", req.body);
+
+	const parseData = JSON.parse(notes);
+
+	parseData.push(req.body);
+
+	fs.writeFileSync(
+		path.join(__dirname, "./data/db.json"),
+		JSON.stringify(parseData)
+	);
+
+	res.json(req.body);
 });
 
 // wild card
