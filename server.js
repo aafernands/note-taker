e165161cce827748c98e563d6164e241fb4f5ff6
Alex Fernandes <alexfernands@outlook.com>
2020-11-21 14:53:46 -0500

    add express to server

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+// Set our port to 8080
+const PORT = process.env.PORT || 8080;
+
+// expose the static  asset folder
+app.use(express.static("assets"));
+
+// Routes
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/notes", function (req, res) {
+	res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+// wild card
+// match any url
+// will match:
+// "notesasdf"
+// "/mynotes"
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// Starts our server
+app.listen(PORT, function () {
+	console.log("Server is listening on PORT: " + PORT);
+});
